019bc88b915ee866e946679019c6f62fc9fc1cef
Yurii <yurimoreau@gmail.com>
2021-08-02 12:36:39 +0300

    jsHint

---
+++ script_test.js
@@ -0,0 +1,9 @@
+"use strict";
+const  numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let  personalMovieDB {
+  let count : numberOfFilms,
+  let movies : null,
+  actors : null,
+  genres:[],
+  privat:false
+};
