6bec10e20cceb04bd63107999f543e4082b3ed76
Yurii <yurimoreau@gmail.com>
2021-08-02 12:51:14 +0300

    Baza filmov

--- script_test.js
+++ script_test.js
@@ -1,9 +1,20 @@
-"use strict";
-const  numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-let  personalMovieDB {
-  let count : numberOfFilms,
-  let movies : null,
-  actors : null,
-  genres:[],
-  privat:false
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
+
+const film1 = prompt("Oдин из последних фильмов", "");
+const ocenka1 = prompt("На сколько его оцените?", "");
+const film2 = prompt("Oдин из последних фильмов", "");
+const ocenka2 = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[film1] = ocenka1;
+
+personalMovieDB.movies[film2] = ocenka2;
+console.log(personalMovieDB);
